58c0ca497e4293d04cc09e32cb262ca8d06ad76e
boleksit <boleksit@gmail.com>
2022-06-16 19:19:25 +0200

    Init DbContext

--- warehouse/Database/AppDbContext.cs
+++ warehouse/Database/AppDbContext.cs
@@ -1,21 +1,18 @@
-﻿using System.Reflection;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using warehouse.Entities;
 
 namespace warehouse.Database;
 
 public class AppDbContext:DbContext
 {
-    public DbSet<AdressEntity> Adresses { get; set; }
+    private string _connectionString =
+        "Data Source=127.0.0.1,1433;Database=WarehouseDB;User Id=sa; Password=PassToSA!;";
+    
+    public DbSet<AddressEntity> Addresses { get; set; }
     public DbSet<ClientEntity> Clients { get; set; }
 
-    public AppDbContext(DbContextOptions options) : base(options) {}
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("Filename=TestDatabase.db", options =>
-        {
-            options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
-        });
-        base.OnConfiguring(optionsBuilder);
-    }
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+      optionsBuilder.UseSqlServer(_connectionString);
+  }
 }
--- warehouse/Program.cs
+++ warehouse/Program.cs
@@ -1,13 +1,12 @@
-using System.Data.SQLite;
 using Microsoft.EntityFrameworkCore;
 using warehouse.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
