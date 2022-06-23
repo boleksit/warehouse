50d1bce214af07e22470e8c80c38b9dd3c048d3c
boleksit <boleksit@gmail.com>
2022-06-23 23:08:19 +0200

    add status entity

---
+++ warehouse/Models/Entities/StatusEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace warehouse.Entities;
+
+public class StatusEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ warehouse/Models/Status.cs
@@ -0,0 +1,7 @@
+﻿namespace warehouse;
+
+public class Status
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
