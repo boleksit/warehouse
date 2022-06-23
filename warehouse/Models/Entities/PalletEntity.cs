8306bafed8ea1c2405852af98c37d3c865f8387b
boleksit <boleksit@gmail.com>
2022-06-23 23:08:11 +0200

    add pallet entity

---
+++ warehouse/Models/Entities/PalletEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace warehouse.Entities;
+
+public class PalletEntity
+{
+    public int Id { get; set; }
+    public int AddressId { get; set; }
+    public int ClientId { get; set; }
+    public int StatusId { get; set; }
+    public virtual List<BoxEntity> Boxes { get; set; } 
+    public virtual AddressEntity Address { get; set; }
+    public virtual ClientEntity Client { get; set; }
+}
---
+++ warehouse/Models/Pallet.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using warehouse.Entities;
+
+namespace warehouse;
+
+public class Pallet
+{
+    public int Id { get; set; }
+    public int Weight { get; set; }
+    public Address Address { get; set; }
+    public Client Client { get; set; }
+    public Status Status { get; set; }
+}
