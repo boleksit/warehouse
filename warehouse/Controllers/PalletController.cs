433769abc63ebe65f8493579821e919444af5b7e
boleksit <boleksit@gmail.com>
2022-06-27 22:31:44 +0200

    add pallet controller

---
+++ warehouse/Controllers/PalletController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using warehouse.Create;
+using warehouse.Modify;
+using warehouse.Services;
+
+namespace warehouse.Controllers;
+[Route("api/pallet")]
+public class PalletController:ControllerBase
+{
+    private readonly IPalletService _palletService;
+
+    public PalletController(IPalletService palletService)
+    {
+        _palletService = palletService;
+    }
+
+    [HttpPost]
+    public ActionResult CreatePallet([FromBody] CreatePallet input)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var result = _palletService.Create(input);
+        return result is null ? NotFound() : Created(result, null);
+    }
+
+    [HttpGet]
+    public ActionResult<List<Pallet>> GetAll()
+    {
+        var result = _palletService.GetAll();
+        return result is null ? NotFound() : Ok(result);
+    }
+    
+    [HttpGet("{palletId}")]
+    public ActionResult<Box> GetById([FromRoute] int palletId)
+    {
+        var result = _palletService.GetById( palletId);
+        return result is null ? NotFound() : Ok(result);
+    }
+    
+    [HttpDelete("{palletId}")]
+    public ActionResult Delete([FromRoute]int palletId)
+    {
+        var result = _palletService.Remove(palletId);
+        return result ? NoContent() : NotFound();
+    }
+    /// <summary>
+    /// Available values: prepared, in transit, in delivery, delivered
+    /// </summary>
+    /// <param name="palletId"></param>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    [HttpPut("{palletId}")]
+    public ActionResult ChangeStatus([FromRoute] int palletId, [FromBody] ModifyStatus input)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var isUpdated = _palletService.ChangeStatus(palletId, input.Status);
+        return isUpdated ? NoContent() : Ok();
+    }
+}
