021a4a47a91e933f74a671b3874f3fc59c7ea977
boleksit <boleksit@gmail.com>
2022-06-28 20:16:20 +0200

    add account service

---
+++ warehouse/Services/AccountService.cs
@@ -0,0 +1,33 @@
+﻿using warehouse.Create;
+using warehouse.Database;
+using warehouse.Entities;
+
+namespace warehouse.Services;
+
+public interface IAccountService
+{
+    void RegisterUser(CreateUser input);
+}
+
+public class AccountService : IAccountService
+{
+    private readonly AppDbContext _context;
+
+    public AccountService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void RegisterUser(CreateUser input)
+    {
+        var user = new UserEntity()
+        {
+            Email = input.Email,
+            Name = input.Name,
+            RoleId = input.RoleId,
+
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+    }
+}
