0ce780283b63ae3633532a48440683a6a6d9888c
boleksit <boleksit@gmail.com>
2022-06-28 20:16:47 +0200

    add migration

--- warehouse/Migrations/20220628171359_Add_Users_and_Roles.Designer.cs
+++ warehouse/Migrations/20220628171359_Add_Users_and_Roles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using warehouse.Database;
 
@@ -11,9 +12,10 @@ using warehouse.Database;
 namespace warehouse.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220628171359_Add_Users_and_Roles")]
+    partial class Add_Users_and_Roles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
