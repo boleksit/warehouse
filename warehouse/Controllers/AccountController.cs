c732929e203616a40877b1e61888b0e48c368539
boleksit <boleksit@gmail.com>
2022-06-28 20:16:08 +0200

    add account controller

---
+++ warehouse/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using warehouse.Create;
+using warehouse.Services;
+
+namespace warehouse.Controllers;
+
+[Route("api/account")]
+[ApiController]
+public class AccountController:ControllerBase
+{
+    private readonly IAccountService _accountService;
+
+    public AccountController(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+    [HttpPost("register")]
+    public ActionResult RegisterUser([FromBody]CreateUser input)
+    {
+        _accountService.RegisterUser(input);
+        return Ok();
+    }
+}
