8ac03762882f1e1516b39b7c82f91b0a18f2d0d7
boleksit <boleksit@gmail.com>
2022-06-28 20:15:25 +0200

    Add user models

---
+++ warehouse/Models/Create/CreateUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace warehouse.Create;
+
+public class CreateUser
+{
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; }
+    public string Name { get; set; }
+    public int RoleId { get; set; } = 3; //default role id is 3 - user
+}
